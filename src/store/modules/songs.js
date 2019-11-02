8d104a242961689c7370efb4aa833249ddd621c1
oskar.zeino <oskar.zeino@tu-dortmund.de>
2019-11-02 01:25:05 +0100

    Added spotify objects store

--- src/store/modules/songs.js
+++ src/store/modules/songs.js
@@ -2,16 +2,24 @@ import s from "../../api/spotifyapiwrapper";
 
 const state = {
   s,
-  userPlaylists: []
+  userPlaylists: [],
+  spotifyObjects: {}
 };
 
 const mutations = {
+  setSpotifyObject(state, { object }) {
+    const { type, id } = object;
+    if (!state[type]) state[type] = {};
+    state[type][id] = object;
+  },
   setUserPlaylists(state, { playlists }) {
     state.userPlaylists = playlists;
   }
 };
 
 const getters = {
+  getSpotifyObject: state => (type, id) =>
+    state.spotifyObjects[type] ? state.spotifyObjects[type][id] : undefined,
   searchUserPlaylists: state => query =>
     state.userPlaylists.findAll(playlist =>
       playlist.name.toLowerCase().includes(query.toLowerCase())
@@ -19,8 +27,14 @@ const getters = {
 };
 
 const actions = {
-  async getUserPlaylists({ commit, state }) {
+  setSpotifyObjects({ commit }, { objects }) {
+    for (const object of objects) {
+      commit("setSpotifyObject", { object });
+    }
+  },
+  async getUserPlaylists({ commit, state, dispatch }) {
     const playlists = await state.s.getWholeUserPlaylists();
+    dispatch("setSpotifyObjects", playlists);
     commit("setUserPlaylists", { playlists });
   }
 };
